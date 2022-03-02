32f25825a6209173d4d81b05dc1e9a99631c1f55
Shawn8901 <shawn8901@googlemail.com>
2022-03-02 19:30:55 +0100

    Use ts3client from upstream as crash is fixed

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,5 @@
 
 final: prev: {
   remmina = prev.callPackage ./remmina { inherit prev; };
-  teamspeak_client = prev.libsForQt5.callPackage ./teamspeak_client { inherit prev; };
   autoadb = prev.callPackage ./autoadb { inherit (inputs.darwin.apple_sdk.frameworks) Security; };
 }
