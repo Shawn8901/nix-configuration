461ed56005781354aba989140a62086de1ff1a35
Shawn8901 <shawn8901@googlemail.com>
2022-04-12 18:55:37 +0200

    Switch back to os channel

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:Shawn8901/nixpkgs/pycurl";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:Shawn8901/nixpkgs/nixos-unstable";
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
