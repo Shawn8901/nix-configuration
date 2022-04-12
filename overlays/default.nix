c13e1047169122dcce1462de685014cf89bea3a1
Shawn8901 <shawn8901@googlemail.com>
2022-04-12 19:00:28 +0200

    Updates portfolio version to 0.57.1

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,4 +10,12 @@ final: prev: {
   nas = prev.callPackage ./shellscripts/nas.nix { };
   backup_server = prev.callPackage ./shellscripts/backup_server.nix { };
   usb-backup-ela = prev.callPackage ./shellscripts/usb-backup-ela.nix { };
+
+  portfolio = prev.portfolio.overrideAttrs (attrs: rec {
+    version = "0.57.1";
+    src = prev.fetchurl {
+    url = "https://github.com/buchen/portfolio/releases/download/${version}/PortfolioPerformance-${version}-linux.gtk.x86_64.tar.gz";
+    sha256 = "sha256-uEEFkHyApf+TObcu+Yo5vBOs2Erq0IXGhbjzlEe8NmI=";
+  };
+  });
 }
