2e18aeaa156eed660e291371f29eb1ff6a01c54e
Shawn8901 <shawn8901@googlemail.com>
2022-06-24 19:25:52 +0200

    nix: set sched policy

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -4,7 +4,6 @@ _:
   environment.systemPackages = [ pkgs.cachix ];
   nix = {
     package = pkgs.nix;
-
     settings = {
       auto-optimise-store = true;
       substituters = [
@@ -17,6 +16,8 @@ _:
         "shawn8901.cachix.org-1:7RAYBGET4e+szLrg86T9PP1vwDp+T99Fq0sTDt3B2DA="
       ];
     };
+    daemonIOSchedClass = "idle";
+    daemonCPUSchedPolicy = "idle";
     extraOptions = "experimental-features = nix-command flakes";
     gc = {
       automatic = true;
