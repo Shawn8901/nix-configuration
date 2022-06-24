9d0769798c055d169337f465decc819f1996f73e
Shawn8901 <shawn8901@googlemail.com>
2022-06-24 18:35:53 +0200

    vscode: added keybindings, disable extentions

--- modules/home-manager/env/vscode.nix
+++ modules/home-manager/env/vscode.nix
@@ -14,39 +14,57 @@ in with lib; {
 
     programs.vscode = {
       enable = true;
-      extensions = (with pkgs.vscode-extensions; [
-        ms-python.python
-        ms-python.vscode-pylance
-        editorconfig.editorconfig
+      /* extensions = (with pkgs.vscode-extensions; [
+           ms-python.python
+           ms-python.vscode-pylance
+           editorconfig.editorconfig
 
-        esbenp.prettier-vscode
+           esbenp.prettier-vscode
 
-        golang.go
+           golang.go
 
-        redhat.vscode-yaml
+           redhat.vscode-yaml
 
-        eamodio.gitlens
+           eamodio.gitlens
 
-        bbenoist.nix
-        brettm12345.nixfmt-vscode
-      ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+           bbenoist.nix
+           brettm12345.nixfmt-vscode
+         ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+           {
+             name = "code-python-isort";
+             publisher = "freakypie";
+             version = "0.0.3";
+             sha256 = "0kzz5k2yh0gk57hgf3ykbrq2qchv209gmbm6milfvnnds0aq3s9r";
+           }
+           {
+             name = "vscode-typescript-tslint-plugin";
+             publisher = "ms-vscode";
+             version = "1.3.3";
+             sha256 = "1xjspcmx5p9x8yq1hzjdkq3acq52nilpd9bm069nsvrzzdh0n891";
+           }
+           {
+             name = "tsimporter";
+             publisher = "pmneo";
+             version = "2.0.1";
+             sha256 = "124jyk9iz3spq8q17z79lqgcwfabbvldcq243xbzbjmbb01ds3i5";
+           }
+         ];
+      */
+      keybindings = [
         {
-          name = "code-python-isort";
-          publisher = "freakypie";
-          version = "0.0.3";
-          sha256 = "0kzz5k2yh0gk57hgf3ykbrq2qchv209gmbm6milfvnnds0aq3s9r";
+          "key" = "ctrl+d";
+          "command" = "-editor.action.addSelectionToNextFindMatch";
+          "when" = "editorFocus";
         }
         {
-          name = "vscode-typescript-tslint-plugin";
-          publisher = "ms-vscode";
-          version = "1.3.3";
-          sha256 = "1xjspcmx5p9x8yq1hzjdkq3acq52nilpd9bm069nsvrzzdh0n891";
+          "key" = "ctrl+d";
+          "command" = "editor.action.deleteLines";
+          "when" = "textInputFocus && !editorReadonly";
         }
         {
-          name = "tsimporter";
-          publisher = "pmneo";
-          version = "2.0.1";
-          sha256 = "124jyk9iz3spq8q17z79lqgcwfabbvldcq243xbzbjmbb01ds3i5";
+          "key" = "ctrl+shift+k";
+          "command" = "-editor.action.deleteLines";
+          "when" = "textInputFocus && !editorReadonly";
         }
       ];
       userSettings = {
