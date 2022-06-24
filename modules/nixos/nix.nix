fd4fa7610bf37d4984b2ac3634fde4cfadbb8437
Shawn8901 <shawn8901@googlemail.com>
2022-06-24 17:27:34 +0200

    nix: added cachix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,23 +1,27 @@
 _:
 { pkgs, ... }: {
+
+  environment.systemPackages = [ pkgs.cachix ];
   nix = {
     package = pkgs.nix;
-    settings.auto-optimise-store = true;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      builders-use-substitutes = true
-      keep-outputs = true
-      keep-derivations = true
-    '';
+
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://shawn8901.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "shawn8901.cachix.org-1:7RAYBGET4e+szLrg86T9PP1vwDp+T99Fq0sTDt3B2DA="
+      ];
+    };
+    extraOptions = "experimental-features = nix-command flakes";
     gc = {
       automatic = true;
       dates = "daily";
       options = "--delete-older-than 7d";
     };
-    binaryCaches =
-      [ "https://cache.nixos.org/" "https://nix-community.cachix.org" ];
-    binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 }
