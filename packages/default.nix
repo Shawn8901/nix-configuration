62180ede94e16f75d592ca03950601f818e92b34
Shawn8901 <shawn8901@googlemail.com>
2022-06-27 17:08:22 +0200

    pointalpha: disabled various stuff in kernel

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,6 @@
 { self, ... }@inputs:
 let
   pkgs = inputs.nixpkgs-stable.legacyPackages.x86_64-linux;
-
   unfree-pkgs = import inputs.nixpkgs-stable {
     system = "x86_64-linux";
     config.allowUnfree = true;
@@ -21,13 +20,66 @@ in {
   usb-backup = pkgs.callPackage ./shellscripts/usb-backup.nix { };
 
   linux_zen_rt = baseKernel.override ({
-    extraConfig = ''
-      PREEMPT y
-      PREEMPT_BUILD y
-      PREEMPT_VOLUNTARY n
-      PREEMPT_COUNT y
-      PREEMPTION y
-    '';
+    argsOverride =  {
+      ignoreConfigErrors = true;
+      structuredExtraConfig = with pkgs.lib; with pkgs.lib.kernel; {
+        PREEMPT = mkForce yes;
+        PREEMPT_BUILD = mkForce yes;
+        PREEMPT_VOLUNTARY = mkForce no;
+        PREEMPT_COUNT = mkForce yes;
+        PREEMPTION = mkForce yes;
+
+        INFINIBAND = mkForce no;
+
+        REISERFS_FS = mkForce no;
+        JFS_FS = mkForce no;
+        XFS_FS = mkForce no;
+        MINIX_FS = mkForce no;
+        ROMFS_FS = mkForce no;
+        ADFS_FS = mkForce no;
+        HFS_FS = mkForce no;
+        HFSPLUS_FS = mkForce no;
+        BEFS_FS = mkForce no;
+        BFS_FS = mkForce no;
+        EFS_FS = mkForce no;
+        JFFS2_FS = mkForce no;
+        CRAMFS = mkForce no;
+        VXFS_FS = mkForce no;
+        HPFS_FS = mkForce no;
+        QNX4FS_FS = mkForce no;
+        SYSV_FS = mkForce no;
+        UFS_FS = mkForce no;
+
+        ISDN = mkForce no;
+        XEN = mkForce no;
+
+        MEDIA_DIGITAL_TV_SUPPORT = mkForce no;
+        MEDIA_ANALOG_TV_SUPPORT = mkForce no;
+        VIDEO_STK1160_COMMON = mkForce no;
+        LIRC = mkForce no;
+        ANDROID = mkForce no;
+
+        CHROME_PLATFORMS = mkForce no;
+
+        VGA_SWITCHEROO = mkForce no;
+        DRM_GMA500 = mkForce no;
+        DRM_GMA600 = mkForce no;
+        DRM_GMA3600 = mkForce no;
+        DRM_VMWGFX_FBCON = mkForce no;
+        DRM_I915_GVT = mkForce no;
+        DRM_I915_GVT_KVMGT = mkForce no;
+
+        FB_NVIDIA_I2C = mkForce no;
+        FB_RIVA_I2C = mkForce no;
+        FB_ATY_CT = mkForce no;
+        FB_ATY_GX = mkForce no;
+        FB_SAVAGE_I2C = mkForce no;
+        FB_SAVAGE_ACCEL = mkForce no;
+        FB_SIS_300 = mkForce no;
+        FB_SIS_315 = mkForce no;
+        FB_3DFX_ACCEL = mkForce no;
+      };
+    };
   });
 
   agenix = inputs.agenix.defaultPackage.x86_64-linux;
