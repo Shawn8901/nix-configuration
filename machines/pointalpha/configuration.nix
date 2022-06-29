07102ffb26159d1e1009a3f47357f135c2d1dd31
Shawn8901 <shawn8901@googlemail.com>
2022-06-29 18:18:59 +0200

    pointalpha: playing around with pipewire settings

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -162,53 +162,99 @@ in {
     zfs.autoScrub.enable = true;
     zfs.autoScrub.pools = [ "rpool" ];
 
-    pipewire = {
+    pipewire = let
+      rate = 48000;
+      quantum = 8192;
+      qr = "${toString quantum}/${toString rate}";
+      rtLimitSoft = 200000;
+      rtLimitHard = 200000;
+    in {
       enable = true;
       pulse.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       media-session.enable = false;
       wireplumber.enable = true;
-
       config.pipewire = {
-            "context.properties" = {"default.clock.min-quantum" = 64;};
+            "context.properties" = {"default.clock.min-quantum" = quantum;};
       };
-      config.pipewire-pulse = let
-        qr = "${toString 64}/${toString 48000}";
-      in
-      {
-        "context.properties" = {};
+      config.pipewire = {
+        "context.properties" = {
+          "link.max-buffers" = 16;
+          "log.level" = 2;
+          "default.clock.rate" = rate;
+          "default.clock.quantum" = quantum;
+          "default.clock.min-quantum" = quantum;
+          "default.clock.max-quantum" = quantum;
+          "core.daemon" = true;
+          "core.name" = "pipewire-0";
+        };
         "context.modules" = [
           {
             name = "libpipewire-module-rtkit";
             args = {
               "nice.level" = -15;
               "rt.prio" = 88;
-              "rt.time.soft" = 200000;
-              "rt.time.hard" = 200000;
+              "rt.time.soft" = rtLimitSoft;
+              "rt.time.hard" = rtLimitHard;
             };
-            flags = ["ifexists" "nofail"];
+            flags = [ "ifexists" "nofail" ];
           }
-          {name = "libpipewire-module-protocol-native";}
-          {name = "libpipewire-module-client-node";}
-          {name = "libpipewire-module-adapter";}
-          {name = "libpipewire-module-metadata";}
+          { name = "libpipewire-module-protocol-native"; }
+          { name = "libpipewire-module-profiler"; }
+          { name = "libpipewire-module-metadata"; }
+          { name = "libpipewire-module-spa-device-factory"; }
+          { name = "libpipewire-module-spa-node-factory"; }
+          { name = "libpipewire-module-client-node"; }
+          { name = "libpipewire-module-client-device"; }
+          {
+            name = "libpipewire-module-portal";
+            flags = [ "ifexists" "nofail" ];
+          }
+          {
+            name = "libpipewire-module-access";
+            args = {};
+          }
+          { name = "libpipewire-module-adapter"; }
+          { name = "libpipewire-module-link-factory"; }
+          { name = "libpipewire-module-session-manager"; }
+        ];
+      };
+      config.pipewire-pulse = {
+          "context.properties" = {
+            "log.level" = 2;
+          };
+          "context.modules" = [
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = rtLimitSoft;
+              "rt.time.hard" = rtLimitHard;
+            };
+            flags = [ "ifexists" "nofail" ];
+          }
+          { name = "libpipewire-module-protocol-native"; }
+          { name = "libpipewire-module-client-node"; }
+          { name = "libpipewire-module-adapter"; }
+          { name = "libpipewire-module-metadata"; }
           {
             name = "libpipewire-module-protocol-pulse";
             args = {
               "pulse.min.req" = qr;
+              "pulse.default.req" = qr;
+              "pulse.max.req" = qr;
               "pulse.min.quantum" = qr;
-              "pulse.min.frag" = qr;
-              "server.address" = ["unix:native"];
+              "pulse.max.quantum" = qr;
+              "server.address" = [ "unix:native" ];
             };
           }
         ];
-
         "stream.properties" = {
           "node.latency" = qr;
           "resample.quality" = 1;
         };
-      };
     };
     printing = {
       enable = true;
