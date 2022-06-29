42c8b41a667458a2521f6abfab1a420ab39f955b
Shawn8901 <shawn8901@googlemail.com>
2022-06-29 13:28:07 +0200

    pointalpha: rt settings from nix-gaming

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -171,16 +171,13 @@ in {
       wireplumber.enable = true;
 
       config.pipewire = {
-        "context.properties" = {
-          "link.max-buffers" = 16;
-          "log.level" = 2;
-          "default.clock.rate" = 48000;
-          "default.clock.quantum" = 32;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
-          "core.daemon" = true;
-          "core.name" = "pipewire-0";
-        };
+            "context.properties" = {"default.clock.min-quantum" = 64;};
+      };
+      config.pipewire-pulse = let
+        qr = "${toString 64}/${toString 48000}";
+      in
+      {
+        "context.properties" = {};
         "context.modules" = [
           {
             name = "libpipewire-module-rtkit";
@@ -190,27 +187,27 @@ in {
               "rt.time.soft" = 200000;
               "rt.time.hard" = 200000;
             };
-            flags = [ "ifexists" "nofail" ];
+            flags = ["ifexists" "nofail"];
           }
-          { name = "libpipewire-module-protocol-native"; }
-          { name = "libpipewire-module-profiler"; }
-          { name = "libpipewire-module-metadata"; }
-          { name = "libpipewire-module-spa-device-factory"; }
-          { name = "libpipewire-module-spa-node-factory"; }
-          { name = "libpipewire-module-client-node"; }
-          { name = "libpipewire-module-client-device"; }
+          {name = "libpipewire-module-protocol-native";}
+          {name = "libpipewire-module-client-node";}
+          {name = "libpipewire-module-adapter";}
+          {name = "libpipewire-module-metadata";}
           {
-            name = "libpipewire-module-portal";
-            flags = [ "ifexists" "nofail" ];
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              "pulse.min.req" = qr;
+              "pulse.min.quantum" = qr;
+              "pulse.min.frag" = qr;
+              "server.address" = ["unix:native"];
+            };
           }
-          {
-            name = "libpipewire-module-access";
-            args = {};
-          }
-          { name = "libpipewire-module-adapter"; }
-          { name = "libpipewire-module-link-factory"; }
-          { name = "libpipewire-module-session-manager"; }
         ];
+
+        "stream.properties" = {
+          "node.latency" = qr;
+          "resample.quality" = 1;
+        };
       };
     };
     printing = {
