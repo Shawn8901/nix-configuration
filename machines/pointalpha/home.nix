eba0e3a09814feb816bb9e920208fbf0265b706c
Shawn8901 <shawn8901@googlemail.com>
2022-07-31 22:31:08 +0200

    noisetorch: lower voice threshold

--- machines/pointalpha/home.nix
+++ machines/pointalpha/home.nix
@@ -70,7 +70,7 @@ in
       noisetorch = {
         enable = true;
         package = self.nixosConfigurations.pointalpha.config.programs.noisetorch.package;
-        threshold = 75;
+        threshold = 30;
         device = "alsa_input.usb-WOER_WOER_20180508-00.iec958-stereo";
         deviceUnit = "dev-snd-by\\x2did-usb\\x2dWOER_WOER_20180508\\x2d00.device";
       };
