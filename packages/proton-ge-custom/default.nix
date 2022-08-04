81513362513b79009bae82616fc66746cb5cd004
Shawn8901 <shawn8901@googlemail.com>
2022-08-04 08:20:22 +0200

    proton: use 7-24

--- packages/proton-ge-custom/default.nix
+++ packages/proton-ge-custom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton7-26";
+  version = "GE-Proton7-24";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "sha256-B50jTRsmvCToCM/os0Ds0EnCULktf218/Y5JJE/QkeQ=";
+    sha256 = "sha256-zUHMO2r9OnGA4MTpcR48DtMlOvuAAKpm0JdQYf87vhU=";
   };
 
   buildCommand = ''
