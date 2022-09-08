9ee508e3073c26d553b755b374a7397ec88c8084
Shawn8901 <shawn8901@googlemail.com>
2022-09-08 23:11:30 +0200

    Include machnix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.utils.follows = "flake-utils";
     };
     nur.url = "github:nix-community/NUR";
     agenix = {
@@ -16,10 +17,24 @@
     nix-vscode-marketplace = {
       url = "github:AmeerTaweel/nix-vscode-marketplace";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.flake-utils.follows = "flake-utils";
     };
+    mach-nix = {
+      url = "github:DavHau/mach-nix/3.5.0";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    pypi-deps-db = {
+      url = github:DavHau/pypi-deps-db;
+      inputs.mach-nix.follows = "mach-nix";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, ... }@inputs:
+  outputs = { self, mach-nix, ... }@inputs:
     let
       system = "x86_64-linux";
       nPkgs = (import inputs.nixpkgs-unstable {
@@ -30,12 +45,13 @@
       sPkgs = (import inputs.nixpkgs-stable { inherit system; });
       uPkgs = (import inputs.nixpkgs-unstable { inherit system; });
       lib = import ./lib (inputs // { inherit lib nPkgs sPkgs uPkgs system; });
+      machNix = import mach-nix { inherit sPkgs; };
     in
     {
       nixosModules = import ./modules/nixos (inputs // { inherit lib system; });
       nixosConfigurations = import ./machines (inputs // { inherit lib nPkgs sPkgs uPkgs system; });
 
-      packages.${system} = (import ./packages (inputs // { inherit system sPkgs uPkgs; }))
+      packages.${system} = import ./packages (inputs // { inherit system sPkgs uPkgs machNix; })
         // lib.nixosConfigurationsAsPackages.configs;
 
       devShells.${system}.default = sPkgs.mkShell {
