ba11d7dd03a891d8ce8a08130bd39ae332659a48
Shawn8901 <shawn8901@googlemail.com>
2022-10-07 21:27:17 +0200

    pointalpha: remove system-config

--- machines/pointalpha/save-darlings.nix
+++ machines/pointalpha/save-darlings.nix
@@ -13,7 +13,6 @@
 
   environment.etc."machine-id".source = "/persist/etc/machine-id";
   environment.etc."nixos".source = "/persist/etc/nixos";
-  environment.etc."NetworkManager/system-connections".source = "/persist/etc/NetworkManager/system-connections";
 
   security.sudo.extraConfig = ''
     Defaults lecture = never
