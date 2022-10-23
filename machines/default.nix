c8669e398afafa2bf3163710bc3417f7b87be19f
Shawn8901 <shawn8901@googlemail.com>
2022-10-23 17:47:53 +0200

    tank,shelter: use unstable

--- machines/default.nix
+++ machines/default.nix
@@ -1,5 +1,5 @@
 { self, lib, ... }@inputs: {
   pointalpha = lib.mkSystem "pointalpha" inputs.nixpkgs-unstable;
-  shelter = lib.mkSystem "shelter" inputs.nixpkgs-stable;
-  tank = lib.mkSystem "tank" inputs.nixpkgs-stable;
+  shelter = lib.mkSystem "shelter" inputs.nixpkgs-unstable;
+  tank = lib.mkSystem "tank" inputs.nixpkgs-unstable;
 }
