9b21fbbd283dc4bd02546475ed880a04b331b458
Shawn8901 <shawn8901@googlemail.com>
2022-11-13 21:14:07 +0100

    proton-ge-custom: 7-38 -> 7-41

--- packages/proton-ge-custom/default.nix
+++ packages/proton-ge-custom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "proton-ge-custom";
-  version = "GE-Proton7-38";
+  version = "GE-Proton7-41";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "sha256-d+ZOJujJZ6BJ1cLYnNd5m84dBXYK/MfyG9HvwFyitdE=";
+    sha256 = "sha256-EPV1d7X5KYV2wZWOzW1JujxBSopvuzwIoY1+mXoswVU=";
   };
 
   passthru.updateScript = ./update.sh;
