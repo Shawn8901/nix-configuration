561323f69ecc5002f5bd07dba05cfc1ebdf65d43
Shawn8901 <shawn8901@googlemail.com>
2022-11-29 23:07:13 +0100

    Removed vmtest again

--- machines/default.nix
+++ machines/default.nix
@@ -4,5 +4,4 @@
   shelter = lib.mkSystem "shelter" inputs.nixpkgs;
   tank = lib.mkSystem "tank" inputs.nixpkgs;
   next = lib.mkSystem "next" inputs.nixpkgs;
-  vmtest = lib.mkSystem "vmtest" inputs.nixpkgs;
 }
--- machines/vmtest/hardware.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, modulesPath, ... }:
-
-{
-  imports = [  (modulesPath + "/virtualisation/virtualbox-image.nix" ) ];
-  nixpkgs.hostPlatform = "x86_64-linux";
-}
