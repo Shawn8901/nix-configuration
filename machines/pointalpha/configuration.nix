c27d79b5d2f4626407b8c05b900391912f68b15b
Shawn8901 <shawn8901@googlemail.com>
2022-12-20 11:39:23 +0100

    pointalpha: No longer enforce wayland backend

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -441,8 +441,6 @@ in
       MOZ_ENABLE_WAYLAND = "1";
       MOZ_DISABLE_RDD_SANDBOX = "1";
       NIXOS_OZONE_WL = "1";
-      SDL_VIDEODRIVER = "wayland";
-      QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       _JAVA_AWT_WM_NONREPARENTING = "1";
     };
