e8d7cf5420568718b4a1729f68d823a8a6d7277c
Shawn8901 <shawn8901@googlemail.com>
2023-01-05 17:01:22 +0100

    pointalpha: use custom nixpkgs

--- machines/default.nix
+++ machines/default.nix
@@ -1,5 +1,5 @@
 { self, lib, ... }@inputs: {
-  pointalpha = lib.mkSystem "pointalpha" inputs.nixpkgs;
+  pointalpha = lib.mkSystem "pointalpha" inputs.nixpkgs-custom;
   tank = lib.mkSystem "tank" inputs.nixpkgs;
   pointjig = lib.mkSystem "pointjig" inputs.nixpkgs-22_11;
   shelter = lib.mkSystem "shelter" inputs.nixpkgs-22_11;
