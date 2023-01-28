48284fc9165d8dd2af45e6d5a2ca55211f5224b9
Shawn8901 <shawn8901@googlemail.com>
2023-01-28 19:13:40 +0100

    cache:  add wg-reresolve-dns

--- flake.nix
+++ flake.nix
@@ -61,11 +61,18 @@
         nixos = mapAttrs (_: cfg: cfg.config.system.build.toplevel) nixosConfigurations;
         release = pkgs.releaseTools.aggregate {
           name = "flake-update";
-          constituents = map (n: "nixos." + n) (builtins.filter (n: !builtins.elem n ["pointalpha" "cache"]) (builtins.attrNames hydraJobs.nixos));
+          constituents = map (n: "nixos." + n) (builtins.filter (n: !builtins.elem n [ "pointalpha" "cache" ]) (builtins.attrNames hydraJobs.nixos));
         };
       };
 
-      packages.${system} = import ./packages (inputs // { inherit pkgs; });
+      packages =
+        let
+          flakePkgs = pkgInstance: (import ./packages (inputs // { pkgs = pkgInstance; }));
+        in
+        {
+          x86_64-linux = flakePkgs pkgs;
+          aarch64-linux = filterAttrs (k: v: k == "wg-reresolve-dns") (flakePkgs nixpkgs.legacyPackages."aarch64-linux");
+        };
 
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
