529e38660f74388e1ad5002ca9a54368ec4a86d2
Shawn8901 <shawn8901@googlemail.com>
2023-02-05 17:07:10 +0100

    deezer: fix menu icon

--- packages/deezer/default.nix
+++ packages/deezer/default.nix
@@ -8,6 +8,7 @@
   nodePackages,
   imagemagick,
   electron_13,
+  fetchFromGitHub,
 }: let
   desktop = makeDesktopItem {
     name = "deezer";
@@ -26,17 +27,24 @@ in
     pname = "deezer";
     version = "5.30.510";
 
-    src = fetchurl {
+    deezer-windows-app = fetchurl {
       url = "https://www.deezer.com/desktop/download/artifact/win32/x86/${finalAttrs.version}";
-      sha256 = "sha256-P/7F4Q+VfDn1B/69dZWWMYj0ri98db525BmcDoRkG44=";
+      hash = "sha256-P/7F4Q+VfDn1B/69dZWWMYj0ri98db525BmcDoRkG44=";
+    };
+
+    linux_patch_upstream = fetchFromGitHub {
+      owner = "aunetx";
+      repo = "deezer-linux";
+      rev = "d4fd8cd41d4c58ef5144f3d7cec79c2679e7dc2b";
+      hash = "sha256-8LiBjn0j863Nlaau+/UvZGJc0CtneWPiY1eiC9be5gw=";
     };
 
     patches = [
-      ./avoid-change-default-texthtml-mime-type.patch
-      ./fix-isDev-usage.patch
-      ./remove-kernel-version-from-user-agent.patch
-      ./start-hidden-in-tray.patch
-      ./quit.patch
+      "${finalAttrs.linux_patch_upstream}/patches/avoid-change-default-texthtml-mime-type.patch"
+      "${finalAttrs.linux_patch_upstream}/patches/fix-isDev-usage.patch"
+      "${finalAttrs.linux_patch_upstream}/patches/remove-kernel-version-from-user-agent.patch"
+      "${finalAttrs.linux_patch_upstream}/patches/start-hidden-in-tray.patch"
+      "${finalAttrs.linux_patch_upstream}/patches/quit.patch"
     ];
 
     nativeBuildInputs = [
@@ -51,7 +59,7 @@ in
 
     unpackPhase = ''
       runHook preUnpack
-      7z x -so ${finalAttrs.src} "\''$PLUGINSDIR/app-32.7z" > app-32.7z
+      7z x -so ${finalAttrs.deezer-windows-app} "\''$PLUGINSDIR/app-32.7z" > app-32.7z
       7z x -y -bsp0 -bso0 app-32.7z
 
       asar extract resources/app.asar resources/app
@@ -76,11 +84,6 @@ in
     buildPhase = ''
       runHook preBuild
 
-      mkdir resources/linux
-      install -Dm644 "resources/win/systray.png" resources/linux/
-
-      convert resources/win/app.ico resources/linux/deezer.png
-
       cd resources
 
       asar pack app app.asar
@@ -95,18 +98,13 @@ in
 
       mkdir -p "$out/share/deezer" "$out/share/deezer/linux" "$out/share/applications" "$out/bin/"  $out/app
 
-      install -Dm644 resources/app.asar "$out/share/deezer/"
-      install -Dm644 resources/win/systray.png "$out/share/deezer/linux/"
+      ln -sf resources/app.asar "$out/share/deezer/"
+      ln -sf ${finalAttrs.linux_patch_upstream}/extra/linux/systray.png "$out/share/deezer/linux/"
 
       for size in 16 32 48 64 128 256; do
           mkdir -p "$out/share/icons/hicolor/''${size}x''${size}/apps/"
-          install -Dm644 resources/win/systray.png  "$out/share/icons/hicolor/''${size}x''${size}/apps/deezer.png"
+          ln -sf ${finalAttrs.linux_patch_upstream}/icons/''${size}x''${size}.png "$out/share/icons/hicolor/''${size}x''${size}/apps/deezer.png"
       done
-
-      mkdir $out/js
-      install -Dm644 resources/main.js "$out/js"
-
-
 
       makeWrapper ${electron_13}/bin/electron $out/bin/deezer \
         --add-flags $out/share/deezer/app.asar \
