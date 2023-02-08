5cf0ff6ed6f0d6b51a3c80646c99459b01506584
Shawn8901 <shawn8901@googlemail.com>
2023-02-08 21:13:12 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
 
     hydraJobs = {
       packages = packages;
-      nixos = mapAttrs (_: cfg: cfg.config.system.build.toplevel) (lib.filterAttrs (name: _: !builtins.elem name ["shelter" "cache"]) nixosConfigurations);
+      nixos = mapAttrs (_: cfg: cfg.config.system.build.toplevel) (nixpkgs.lib.filterAttrs (name: _: !builtins.elem name ["shelter" "cache"]) nixosConfigurations);
       release = pkgs.releaseTools.aggregate {
         name = "flake-update";
         constituents = map (n: "nixos." + n) (builtins.filter (n: !builtins.elem n ["pointalpha" "shelter" "cache"]) (builtins.attrNames hydraJobs.nixos));
