55722f9d6bdc6be56d7e08f85e63db8d275496d6
Shawn8901 <shawn8901@googlemail.com>
2023-02-26 13:56:36 +0100

    tank: dont use custom-nixpkgs

--- machines/default.nix
+++ machines/default.nix
@@ -4,7 +4,7 @@
   ...
 } @ inputs: {
   pointalpha = lib.mkSystem "pointalpha" inputs.nixpkgs-custom;
-  tank = lib.mkSystem "tank" inputs.nixpkgs-custom;
+  tank = lib.mkSystem "tank" inputs.nixpkgs;
   pointjig = lib.mkSystem "pointjig" inputs.nixpkgs-22_11;
   shelter = lib.mkSystem "shelter" inputs.nixpkgs-22_11;
   next = lib.mkSystem "next" inputs.nixpkgs-22_11;
