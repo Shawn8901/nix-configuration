d461c142da1b46a684fb762a9ba047f4ea99f4e8
Shawn8901 <shawn8901@googlemail.com>
2023-02-26 12:49:50 +0100

    tank: add pointalpha as singleJob Builder

--- machines/tank/configuration.nix
+++ machines/tank/configuration.nix
@@ -755,13 +755,13 @@ in {
       maxJobs = 2;
       inherit sshUser sshKey;
     }
-    # {
-    #   hostName = "pointalpha";
-    #   systems = ["x86_64-linux" "i686-linux"];
-    #   maxJobs = 2;
-    #   supportedFeatures = ["gccarch-x86-64-v2" "gccarch-x86-64-v3" "benchmark" "big-parallel" "kvm" "nixos-test"];
-    #   inherit sshUser sshKey;
-    # }
+    {
+      hostName = "pointalpha";
+      systems = ["x86_64-linux" "i686-linux"];
+      maxJobs = 1;
+      supportedFeatures = ["gccarch-x86-64-v2" "gccarch-x86-64-v3" "benchmark" "big-parallel" "kvm" "nixos-test"];
+      inherit sshUser sshKey;
+    }
   ];
   nix.settings.max-jobs = 4;
   nix.extraOptions = ''
