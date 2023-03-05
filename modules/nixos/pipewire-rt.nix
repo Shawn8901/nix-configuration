049660a9ba835ad461ca387a1a879820a7593964
Shawn8901 <shawn8901@googlemail.com>
2023-03-05 14:42:37 +0100

    pointalpha/zenbook: externalize pipewire-rt config

--- modules/nixos/pipewire-rt.nix
+++ modules/nixos/pipewire-rt.nix
@@ -34,95 +34,95 @@ in {
         default = 300000;
       };
     };
-    config = lib.mkIf cfg.enable {
-      security.rtkit.enable = true;
+  };
+  config = lib.mkIf cfg.enable {
+    security.rtkit.enable = true;
 
-      services.pipewire = let
-        qr = "${toString cfg.quantum}/${toString cfg.rate}";
-      in {
-        enable = true;
-        pulse.enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        media-session.enable = false;
-        wireplumber.enable = true;
-        config.pipewire = {
-          "context.properties" = {
-            "link.max-buffers" = 16;
-            "log.level" = 2;
-            "default.clock.rate" = cfg.rate;
-            "default.clock.quantum" = cfg.quantum;
-            "default.clock.min-quantum" = cfg.minQuantum;
-            "default.clock.max-quantum" = cfg.maxQuantum;
-            "core.daemon" = true;
-            "core.name" = "pipewire-0";
-          };
-          "context.modules" = [
-            {
-              name = "libpipewire-module-rtkit";
-              args = {
-                "nice.level" = -15;
-                "rt.prio" = 88;
-                "rt.time.soft" = cfg.rtLimitSoft;
-                "rt.time.hard" = cfg.rtLimitHard;
-              };
-              flags = ["ifexists" "nofail"];
-            }
-            {name = "libpipewire-module-protocol-native";}
-            {name = "libpipewire-module-profiler";}
-            {name = "libpipewire-module-metadata";}
-            {name = "libpipewire-module-spa-device-factory";}
-            {name = "libpipewire-module-spa-node-factory";}
-            {name = "libpipewire-module-client-node";}
-            {name = "libpipewire-module-client-device";}
-            {
-              name = "libpipewire-module-portal";
-              flags = ["ifexists" "nofail"];
-            }
-            {
-              name = "libpipewire-module-access";
-              args = {};
-            }
-            {name = "libpipewire-module-adapter";}
-            {name = "libpipewire-module-link-factory";}
-            {name = "libpipewire-module-session-manager";}
-          ];
+    services.pipewire = let
+      qr = "${toString cfg.quantum}/${toString cfg.rate}";
+    in {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      media-session.enable = false;
+      wireplumber.enable = true;
+      config.pipewire = {
+        "context.properties" = {
+          "link.max-buffers" = 16;
+          "log.level" = 2;
+          "default.clock.rate" = cfg.rate;
+          "default.clock.quantum" = cfg.quantum;
+          "default.clock.min-quantum" = cfg.minQuantum;
+          "default.clock.max-quantum" = cfg.maxQuantum;
+          "core.daemon" = true;
+          "core.name" = "pipewire-0";
         };
-        config.pipewire-pulse = {
-          "context.properties" = {
-            "log.level" = 2;
-          };
-          "context.modules" = [
-            {
-              name = "libpipewire-module-rtkit";
-              args = {
-                "nice.level" = -15;
-                "rt.prio" = 88;
-                "rt.time.soft" = cfg.rtLimitSoft;
-                "rt.time.hard" = cfg.rtLimitHard;
-              };
-              flags = ["ifexists" "nofail"];
-            }
-            {name = "libpipewire-module-protocol-native";}
-            {name = "libpipewire-module-client-node";}
-            {name = "libpipewire-module-adapter";}
-            {name = "libpipewire-module-metadata";}
-            {
-              name = "libpipewire-module-protocol-pulse";
-              args = {
-                "pulse.min.req" = qr;
-                "pulse.default.req" = qr;
-                "pulse.max.req" = qr;
-                "pulse.min.quantum" = qr;
-                "pulse.max.quantum" = qr;
-                "server.address" = ["unix:native"];
-              };
-            }
-          ];
-          "stream.properties" = {
-            "node.latency" = qr;
-            "resample.quality" = 1;
-          };
+        "context.modules" = [
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = cfg.rtLimitSoft;
+              "rt.time.hard" = cfg.rtLimitHard;
+            };
+            flags = ["ifexists" "nofail"];
+          }
+          {name = "libpipewire-module-protocol-native";}
+          {name = "libpipewire-module-profiler";}
+          {name = "libpipewire-module-metadata";}
+          {name = "libpipewire-module-spa-device-factory";}
+          {name = "libpipewire-module-spa-node-factory";}
+          {name = "libpipewire-module-client-node";}
+          {name = "libpipewire-module-client-device";}
+          {
+            name = "libpipewire-module-portal";
+            flags = ["ifexists" "nofail"];
+          }
+          {
+            name = "libpipewire-module-access";
+            args = {};
+          }
+          {name = "libpipewire-module-adapter";}
+          {name = "libpipewire-module-link-factory";}
+          {name = "libpipewire-module-session-manager";}
+        ];
+      };
+      config.pipewire-pulse = {
+        "context.properties" = {
+          "log.level" = 2;
+        };
+        "context.modules" = [
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = cfg.rtLimitSoft;
+              "rt.time.hard" = cfg.rtLimitHard;
+            };
+            flags = ["ifexists" "nofail"];
+          }
+          {name = "libpipewire-module-protocol-native";}
+          {name = "libpipewire-module-client-node";}
+          {name = "libpipewire-module-adapter";}
+          {name = "libpipewire-module-metadata";}
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              "pulse.min.req" = qr;
+              "pulse.default.req" = qr;
+              "pulse.max.req" = qr;
+              "pulse.min.quantum" = qr;
+              "pulse.max.quantum" = qr;
+              "server.address" = ["unix:native"];
+            };
+          }
+        ];
+        "stream.properties" = {
+          "node.latency" = qr;
+          "resample.quality" = 1;
         };
       };
     };
