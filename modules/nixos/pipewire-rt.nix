7658d93c99d467ebb8cca5c867c54eab47bf1105
Shawn8901 <shawn8901@googlemail.com>
2023-03-15 20:20:16 +0100

    pipewire: generate config files manually

--- modules/nixos/pipewire-rt.nix
+++ modules/nixos/pipewire-rt.nix
@@ -5,6 +5,88 @@
   ...
 }: let
   cfg = config.env.pipewire-rt;
+  jsonGenerate = pkgs.formats.json {};
+  qr = "${toString cfg.quantum}/${toString cfg.rate}";
+
+  customConfig = jsonGenerate.generate "99-custom.conf" {
+    "context.properties" = {
+      "link.max-buffers" = 16;
+      "log.level" = 2;
+      "default.clock.rate" = cfg.rate;
+      "default.clock.quantum" = cfg.quantum;
+      "default.clock.min-quantum" = cfg.minQuantum;
+      "default.clock.max-quantum" = cfg.maxQuantum;
+      "core.daemon" = true;
+      "core.name" = "pipewire-0";
+    };
+    "context.modules" = [
+      {
+        name = "libpipewire-module-rtkit";
+        args = {
+          "nice.level" = -15;
+          "rt.prio" = 88;
+          "rt.time.soft" = cfg.rtLimitSoft;
+          "rt.time.hard" = cfg.rtLimitHard;
+        };
+        flags = ["ifexists" "nofail"];
+      }
+      {name = "libpipewire-module-protocol-native";}
+      {name = "libpipewire-module-profiler";}
+      {name = "libpipewire-module-metadata";}
+      {name = "libpipewire-module-spa-device-factory";}
+      {name = "libpipewire-module-spa-node-factory";}
+      {name = "libpipewire-module-client-node";}
+      {name = "libpipewire-module-client-device";}
+      {
+        name = "libpipewire-module-portal";
+        flags = ["ifexists" "nofail"];
+      }
+      {
+        name = "libpipewire-module-access";
+        args = {};
+      }
+      {name = "libpipewire-module-adapter";}
+      {name = "libpipewire-module-link-factory";}
+      {name = "libpipewire-module-session-manager";}
+    ];
+  };
+
+  customPulseConfig = jsonGenerate.generate "99-pulse-custom.conf" {
+    "context.properties" = {
+      "log.level" = 2;
+    };
+    "context.modules" = [
+      {
+        name = "libpipewire-module-rtkit";
+        args = {
+          "nice.level" = -15;
+          "rt.prio" = 88;
+          "rt.time.soft" = cfg.rtLimitSoft;
+          "rt.time.hard" = cfg.rtLimitHard;
+        };
+        flags = ["ifexists" "nofail"];
+      }
+      {name = "libpipewire-module-protocol-native";}
+      {name = "libpipewire-module-client-node";}
+      {name = "libpipewire-module-adapter";}
+      {name = "libpipewire-module-metadata";}
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          "pulse.min.req" = qr;
+          "pulse.default.req" = qr;
+          "pulse.max.req" = qr;
+          "pulse.min.quantum" = qr;
+          "pulse.max.quantum" = qr;
+          "server.address" = ["unix:native"];
+        };
+      }
+    ];
+    "stream.properties" = {
+      "node.latency" = qr;
+      "resample.quality" = 1;
+    };
+  };
 in {
   options = {
     env.pipewire-rt = {
@@ -38,93 +120,16 @@ in {
   config = lib.mkIf cfg.enable {
     security.rtkit.enable = true;
 
-    services.pipewire = let
-      qr = "${toString cfg.quantum}/${toString cfg.rate}";
-    in {
+    services.pipewire = {
       enable = true;
       pulse.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       media-session.enable = false;
       wireplumber.enable = true;
-      config.pipewire = {
-        "context.properties" = {
-          "link.max-buffers" = 16;
-          "log.level" = 2;
-          "default.clock.rate" = cfg.rate;
-          "default.clock.quantum" = cfg.quantum;
-          "default.clock.min-quantum" = cfg.minQuantum;
-          "default.clock.max-quantum" = cfg.maxQuantum;
-          "core.daemon" = true;
-          "core.name" = "pipewire-0";
-        };
-        "context.modules" = [
-          {
-            name = "libpipewire-module-rtkit";
-            args = {
-              "nice.level" = -15;
-              "rt.prio" = 88;
-              "rt.time.soft" = cfg.rtLimitSoft;
-              "rt.time.hard" = cfg.rtLimitHard;
-            };
-            flags = ["ifexists" "nofail"];
-          }
-          {name = "libpipewire-module-protocol-native";}
-          {name = "libpipewire-module-profiler";}
-          {name = "libpipewire-module-metadata";}
-          {name = "libpipewire-module-spa-device-factory";}
-          {name = "libpipewire-module-spa-node-factory";}
-          {name = "libpipewire-module-client-node";}
-          {name = "libpipewire-module-client-device";}
-          {
-            name = "libpipewire-module-portal";
-            flags = ["ifexists" "nofail"];
-          }
-          {
-            name = "libpipewire-module-access";
-            args = {};
-          }
-          {name = "libpipewire-module-adapter";}
-          {name = "libpipewire-module-link-factory";}
-          {name = "libpipewire-module-session-manager";}
-        ];
-      };
-      config.pipewire-pulse = {
-        "context.properties" = {
-          "log.level" = 2;
-        };
-        "context.modules" = [
-          {
-            name = "libpipewire-module-rtkit";
-            args = {
-              "nice.level" = -15;
-              "rt.prio" = 88;
-              "rt.time.soft" = cfg.rtLimitSoft;
-              "rt.time.hard" = cfg.rtLimitHard;
-            };
-            flags = ["ifexists" "nofail"];
-          }
-          {name = "libpipewire-module-protocol-native";}
-          {name = "libpipewire-module-client-node";}
-          {name = "libpipewire-module-adapter";}
-          {name = "libpipewire-module-metadata";}
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {
-              "pulse.min.req" = qr;
-              "pulse.default.req" = qr;
-              "pulse.max.req" = qr;
-              "pulse.min.quantum" = qr;
-              "pulse.max.quantum" = qr;
-              "server.address" = ["unix:native"];
-            };
-          }
-        ];
-        "stream.properties" = {
-          "node.latency" = qr;
-          "resample.quality" = 1;
-        };
-      };
     };
+
+    environment.etc."pipewire/pipewire.conf.d/99-custom.conf".source = customConfig;
+    environment.etc."pipewire/pipewire.conf.d/99-pulse-custom.conf".source = customPulseConfig;
   };
 }
