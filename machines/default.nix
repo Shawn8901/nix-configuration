f53a0d69644c4897a4128bf296a1a9ae4b94a34d
Shawn8901 <shawn8901@googlemail.com>
2023-03-17 22:51:17 +0100

    use nixpkgs-custom always

--- machines/default.nix
+++ machines/default.nix
@@ -4,10 +4,10 @@
   ...
 } @ inputs: {
   pointalpha = lib.mkSystem "pointalpha" inputs.nixpkgs-custom;
-  tank = lib.mkSystem "tank" inputs.nixpkgs;
+  tank = lib.mkSystem "tank" inputs.nixpkgs-custom;
   pointjig = lib.mkSystem "pointjig" inputs.nixpkgs-22_11;
   shelter = lib.mkSystem "shelter" inputs.nixpkgs-22_11;
   next = lib.mkSystem "next" inputs.nixpkgs-22_11;
   cache = lib.mkSystem "cache" inputs.nixpkgs-custom;
-  zenbook = lib.mkSystem "zenbook" inputs.nixpkgs;
+  zenbook = lib.mkSystem "zenbook" inputs.nixpkgs-custom;
 }
