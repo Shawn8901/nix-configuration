b8e1f4792c546b503c55738154989e507ca92e06
Shawn8901 <shawn8901@googlemail.com>
2023-03-17 23:00:02 +0100

    tank: build x86-64-v3

--- machines/tank/hardware.nix
+++ machines/tank/hardware.nix
@@ -9,8 +9,11 @@
 in {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
-  nixpkgs.hostPlatform.system = "x86_64-linux";
   nix.settings.system-features = ["gccarch-x86-64-v2" "gccarch-x86-64-v3" "benchmark" "big-parallel" "kvm" "nixos-test"];
+  nixpkgs.hostPlatform = {
+    gcc.arch = "x86-64-v3";
+    system = "x86_64-linux";
+  };
 
   boot = {
     initrd.availableKernelModules = ["ahci" "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod"];
