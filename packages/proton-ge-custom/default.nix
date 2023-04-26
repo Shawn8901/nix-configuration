f98162cc88d786c478aef93f3fb916a6dc5b72c9
Shawn8901 <shawn8901@googlemail.com>
2023-04-26 21:03:34 +0200

    proton-ge-custom: set proper maintainer

--- packages/proton-ge-custom/default.nix
+++ packages/proton-ge-custom/default.nix
@@ -1,8 +1,8 @@
-{ stdenv
-, lib
-, fetchurl
-, writeScript
-,
+{
+  stdenv,
+  lib,
+  fetchurl,
+  writeScript,
 }:
 stdenv.mkDerivation (finalAttrs: {
   name = "proton-ge-custom";
@@ -24,7 +24,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Compatibility tool for Steam Play based on Wine and additional components";
     homepage = "https://github.com/GloriousEggroll/proton-ge-custom";
     license = licenses.bsd3;
-    platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ ajs124 ];
+    platforms = ["x86_64-linux"];
+    maintainers = with maintainers; [shawn8901];
   };
 })
