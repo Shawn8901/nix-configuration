4437606c63c85fc9c328c0122cc8bb2c99b6e63d
Shawn8901 <shawn8901@googlemail.com>
2023-06-14 22:03:33 +0200

    optimized: remove unneeded znver1

--- profiles/nixos/optimized.nix
+++ profiles/nixos/optimized.nix
@@ -3,7 +3,7 @@
 in {
   #aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sse sse2 sse3 sse4_1 sse4_2 ssse3
 
-  nix.settings.system-features = ["gccarch-x86-64-v3" "gccarch-znver1" "benchmark" "big-parallel" "kvm" "nixos-test"];
+  nix.settings.system-features = ["gccarch-x86-64-v3" "benchmark" "big-parallel" "kvm" "nixos-test"];
   nixpkgs.hostPlatform.gcc.arch = "x86-64-v3";
 
   nixpkgs.config.packageOverrides = pkgs: {
