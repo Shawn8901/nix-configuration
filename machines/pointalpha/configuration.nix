5e4a1ae80d2fd064ca97fbb6926bd4aa646facbe
Shawn8901 <shawn8901@googlemail.com>
2023-11-09 23:47:51 +0100

    pointalpha: add extra udev rules to flash Voyager

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -36,6 +36,12 @@ in {
 
   services = {
     udev.packages = [ pkgs.libmtp.out ];
+    udev.extraRules = ''
+      # Keymapp / Wally Flashing rules for the Moonlander and Planck EZ
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
+      # Keymapp Flashing rules for the Voyager
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+    '';
     openssh = {
       enable = true;
       hostKeys = [
