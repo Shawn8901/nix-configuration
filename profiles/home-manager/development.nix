2d1b814ef8bc529b58b39fa0e77a410436475160
Shawn8901 <shawn8901@googlemail.com>
2023-11-12 23:37:43 +0100

    development: use vim as default editor

--- profiles/home-manager/development.nix
+++ profiles/home-manager/development.nix
@@ -109,6 +109,39 @@ in {
     };
   };
 
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = ''
+      set nocompatible
+      filetype indent on
+      syntax on
+      set hidden
+      set wildmenu
+      set showcmd
+      set incsearch
+      set hlsearch
+      set backspace=indent,eol,start
+      set autoindent
+      set nostartofline
+      set ruler
+      set laststatus=2
+      set confirm
+      set visualbell
+      set t_vb=
+      set cmdheight=2
+      set number
+      set notimeout ttimeout ttimeoutlen=200
+      set pastetoggle=<F11>
+      set tabstop=8
+      set shiftwidth=4
+      set softtabstop=4
+      set expandtab
+      map Y y$
+      nnoremap <C-L> :nohl<CR><C-L>
+    '';
+  };
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
