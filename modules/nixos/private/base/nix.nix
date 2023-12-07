32f6acbd85fa15b335e723fc4602cb81af4133f2
Shawn8901 <shawn8901@googlemail.com>
2023-12-07 23:06:09 +0100

    all: override nix version <23.11

--- modules/nixos/private/base/nix.nix
+++ modules/nixos/private/base/nix.nix
@@ -1,7 +1,7 @@
 { self, pkgs, lib, config, inputs', ... }:
 let
   inherit (pkgs.hostPlatform) system;
-  inherit (lib) genAttrs;
+  inherit (lib) genAttrs mkIf mkDefault mkForce versionOlder;
 in {
   sops.secrets = {
     nix-gh-token-ro = {
@@ -17,7 +17,8 @@ in {
   };
 
   nix = {
-    package = lib.mkDefault pkgs.nixVersions.nix_2_17;
+    package = mkIf (versionOlder config.system.nixos.release "23.11")
+      pkgs.nixVersions.nix_2_17;
     settings = {
       auto-optimise-store = true;
       allow-import-from-derivation = false;
@@ -29,10 +30,10 @@ in {
         "nixos:Uj2EOFZu9/Y6r6qVlxeCyiGVqyz30fMybzT3kBDsPg8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      cores = lib.mkDefault 6;
-      max-jobs = lib.mkDefault 2;
+      cores = mkDefault 6;
+      max-jobs = mkDefault 2;
       experimental-features = "nix-command flakes";
-      netrc-file = lib.mkForce config.sops.secrets.nix-netrc-ro.path;
+      netrc-file = mkForce config.sops.secrets.nix-netrc-ro.path;
     };
     extraOptions = ''
       !include ${config.sops.secrets.nix-gh-token-ro.path}
@@ -40,13 +41,13 @@ in {
       max-free = ${toString (5 * 1024 * 1024 * 1024)}
       pure-eval = true
     '';
-    nrBuildUsers = lib.mkForce 16;
+    nrBuildUsers = mkForce 16;
     daemonIOSchedClass = "idle";
     daemonCPUSchedPolicy = "idle";
     gc = {
       automatic = true;
       dates = "weekly";
-      options = lib.mkDefault "--delete-older-than 7d";
+      options = mkDefault "--delete-older-than 7d";
     };
   };
 }
