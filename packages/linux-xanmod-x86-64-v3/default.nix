3e07d133010bfd8607b6ed54411a145d03ab1ae9
Shawn8901 <shawn8901@googlemail.com>
2024-02-18 16:57:15 +0100

    xanmod: use stable as its now the 6.6 kernel

--- packages/linux-xanmod-x86-64-v3/default.nix
+++ packages/linux-xanmod-x86-64-v3/default.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 let inherit (lib.kernel) yes no;
-in pkgs.linux_xanmod_stable.override (finalAttrs: {
+in pkgs.linux_xanmod.override (finalAttrs: {
   structuredExtraConfig = finalAttrs.structuredExtraConfig // {
     GENERIC_CPU = no;
     GENERIC_CPU3 = yes;
