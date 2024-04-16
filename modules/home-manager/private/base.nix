09d86daeae248a53b7dd6fa4e1989212c9cc9830
Shawn8901 <shawn8901@googlemail.com>
2024-04-16 20:44:51 +0200

    home-manager: add weekly cleanup

--- modules/home-manager/private/base.nix
+++ modules/home-manager/private/base.nix
@@ -1,12 +1,24 @@
-_: {
-  home.stateVersion = "23.05";
+{ lib, osConfig, ... }:
+let
+  inherit (lib) versionOlder;
+in
+lib.mkMerge [
+  {
+    home.stateVersion = "23.05";
 
-  programs.zsh = {
-    enable = true;
-  };
-  programs.dircolors = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  manual.manpages.enable = false;
-}
+    programs.zsh = {
+      enable = true;
+    };
+    programs.dircolors = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    manual.manpages.enable = false;
+  }
+  (lib.optionalAttrs (!versionOlder osConfig.system.nixos.release "24.05") ({
+    nix.gc = {
+      automatic = true;
+      options = "-d";
+    };
+  }))
+]
