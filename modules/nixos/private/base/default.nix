bc830d01024d6f3ef00a7a67fac8ee87668b74b2
Shawn8901 <shawn8901@googlemail.com>
2024-05-02 20:18:38 +0200

    default: udev rule for zfs is applied upstream, drop it

--- modules/nixos/private/base/default.nix
+++ modules/nixos/private/base/default.nix
@@ -52,10 +52,6 @@ in
         SystemMaxUse=100M
         SystemMaxFileSize=50M
       '';
-
-      udev.extraRules = optionalString (config.boot.supportedFilesystems.zfs or false) ''
-        ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
-      '';
     }
     (optionalAttrs (!versionOlder config.system.nixos.release "24.05") {
       dbus.implementation = "broker";
