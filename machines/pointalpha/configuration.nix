40f27303b6e025f081e06a4c2f36be492a07aade
Shawn8901 <shawn8901@googlemail.com>
2024-05-11 00:46:45 +0200

    pointalpha: add attic client

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -154,7 +154,10 @@ in
 
   nix.settings.keep-outputs = true;
   environment = {
-    systemPackages = [ pkgs.cifs-utils ];
+    systemPackages = [
+      pkgs.cifs-utils
+      inputs'.attic.packages.attic-client
+    ];
     etc = {
       "samba/credentials_ela".source = secrets.samba-ela.path;
       "samba/credentials_shawn".source = secrets.samba.path;
