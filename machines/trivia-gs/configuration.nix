7c010bc900265a2c325e4001c6120253f2fde57e
Shawn8901 <shawn8901@googlemail.com>
2024-05-12 23:06:59 +0200

    trivia-gs: use stalwart for acme provisioning

--- machines/trivia-gs/configuration.nix
+++ machines/trivia-gs/configuration.nix
@@ -43,57 +43,21 @@ in
     wait-online.anyInterface = true;
   };
 
-  systemd.services.stalwart-mail.serviceConfig = {
-    # Hack to read acme certificate from nginx
-    Group = "nginx";
-  };
-
   services = {
     fstrim.enable = true;
-    stalwart-mail = rec {
+    stalwart-mail = {
       enable = true;
       package = fPkgs.stalwart-mail;
       environmentFile = secrets.stalwart-fallback-admin.path;
       hostname = "mail.trivia-gs.de";
-      # http = {
-      #   listenAddress = "127.0.0.1";
-      #   port = 8080;
-      #   openFirewall = false;
-      #   tls = false;
-      # };
-      # settings = {
-      #   certificate.default = {
-      #     private-key = "%{file:/var/lib/acme/${hostname}/key.pem}%";
-      #     cert = "%{file:/var/lib/acme/${hostname}/cert.pem}%";
-      #     default = true;
-      #   };
-      #   server.http.use-x-forwarded = true;
-      # };
-    };
-    nginx = {
-      enable = false;
-      package = pkgs.nginxQuic;
-      virtualHosts."${config.services.stalwart-mail.hostname}" = {
-        serverName = config.services.stalwart-mail.hostname;
-        forceSSL = true;
-        enableACME = true;
-        http3 = true;
-        kTLS = true;
-        locations = {
-          "/" = {
-            proxyPass = "http://localhost:8080";
-            recommendedProxySettings = true;
-          };
-        };
+      settings.acme."letsencrypt" = {
+        directory = "https://acme-staging-v02.api.letsencrypt.org/directory";
+        challenge = "tls-alpn-01";
+        contact = [ "barannikov.de@gmail.com" ];
+        cache = "%{BASE_PATH}%/etc/acme";
+        domains = [ "${config.services.stalwart-mail.hostname}" ];
+        default = true;
       };
-    };
-  };
-
-  security = {
-    acme = {
-      acceptTerms = true;
-      defaults.email = "barannikov.de@gmail.com";
-      # certs."${config.services.stalwart-mail.hostname}".reloadServices = [ "stalwart-mail" ];
     };
   };
 
