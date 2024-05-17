1b0ea5811a4d72c0fc5461a93e663f95b62f4ae9
Shawn8901 <shawn8901@googlemail.com>
2024-05-17 22:41:21 +0200

    pointjig: optimize postgres setting

--- machines/pointjig/configuration.nix
+++ machines/pointjig/configuration.nix
@@ -65,9 +65,19 @@ in
     fstrim.enable = true;
     postgresql.settings = {
       max_connections = 200;
-      effective_cache_size = "256MB";
-      shared_buffers = "256MB";
-      work_mem = "16MB";
+      shared_buffers = "1GB";
+      effective_cache_size = "3GB";
+      maintenance_work_mem = "256MB";
+      checkpoint_completion_target = 0.9;
+      wal_buffers = "16MB";
+      default_statistics_target = 100;
+      random_page_cost = 4;
+      effective_io_concurrency = 2;
+      work_mem = "1310kB";
+      huge_pages = "off";
+      min_wal_size = "1GB";
+      max_wal_size = "4GB";
+
       track_activities = true;
       track_counts = true;
       track_io_timing = true;
