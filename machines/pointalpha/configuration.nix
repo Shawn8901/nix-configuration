32bc96e039ec369121d4995d832e6473e85f5152
Shawn8901 <shawn8901@googlemail.com>
2024-05-18 19:15:44 +0200

    pointalpha: remove temporaya hostname pinning

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -36,7 +36,6 @@ in
       "2a05:bec0:1:16::114" = [ "pointjig" ];
       "78.128.127.235" = [ "shelter" ];
       "2a01:8740:1:e4::2cd3" = [ "shelter" ];
-      "134.255.226.117" = [ "triva-gs.de" ];
     };
     dhcpcd.enable = false;
     useNetworkd = false;
