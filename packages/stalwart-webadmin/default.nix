d4980a18478340e5fb954d2339f9648005e14734
Shawn8901 <shawn8901@googlemail.com>
2024-06-10 22:55:11 +0200

    stalwart-webadmin: init at 0.1.5

---
+++ packages/stalwart-webadmin/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  fetchurl,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "stalwart-webadmin";
+  version = "0.1.5";
+
+  src = fetchurl {
+    url = "https://github.com/stalwartlabs/webadmin/releases/download/v${finalAttrs.version}/webadmin.zip";
+    hash = "sha256-+FIdiA4iMur8GS46mpTAQpOQYCb11O9oZoiijQ/q6xY=";
+  };
+
+  outputs = [
+    "out"
+    "webadmin"
+  ];
+
+  buildCommand = ''
+    runHook preBuild
+
+    # Make it impossible to add to an environment. You should use the appropriate NixOS option.
+    # Also leave some breadcrumbs in the file.
+    echo "${finalAttrs.pname} should not be installed into environments. Please use  services.stalwart-mail.settings.config.resource.webadmin instead." > $out
+
+    mkdir -p $webadmin
+    ln -s $src $webadmin/webadmin.zip
+
+    runHook postBuild
+  '';
+
+  passthru.runUpdate = true;
+
+  meta = with lib; {
+    homepage = "https://github.com/stalwartlabs/webadmin";
+    description = "Web-based admin for Stalwart Mail Server ";
+    license = licenses.agpl3Only;
+    maintainers = with maintainers; [ shawn8901 ];
+    platforms = lib.platforms.all;
+  };
+})
