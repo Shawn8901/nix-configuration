0bf20a8b89530721c33d4a81e66cdc737c8c2e53
Shawn8901 <shawn8901@googlemail.com>
2024-10-09 20:12:56 +0200

    tank: fix failing of pointalpha-online script

--- machines/tank/configuration.nix
+++ machines/tank/configuration.nix
@@ -96,11 +96,14 @@ in
         {
           script = ''
             if ${pkgs.iputils}/bin/ping -c1 -w 1 pointalpha > /dev/null; then
-              grep pointalpha /tmp/hyda/dynamic-machines > /dev/null || \
-              echo "ssh://root@pointalpha x86_64-linux,i686-linux ${secrets.ssh-builder-key.path} ${toString maxJobs} 4 ${concatStringsSep "," systemFeatures} - -" >  /tmp/hyda/dynamic-machines \
-              && echo "Added pointalpha to dynamic build machines"
+              if ! grep pointalpha /tmp/hyda/dynamic-machines > /dev/null; then
+                echo "ssh://root@pointalpha x86_64-linux,i686-linux ${secrets.ssh-builder-key.path} ${toString maxJobs} 4 ${concatStringsSep "," systemFeatures} - -" >  /tmp/hyda/dynamic-machines \
+                echo "Added pointalpha to dynamic build machines"
+              fi
             else
-              grep pointalpha /tmp/hyda/dynamic-machines > /dev/null && echo "" > /tmp/hyda/dynamic-machines && echo "Cleared dynamic build machines"
+              if grep pointalpha /tmp/hyda/dynamic-machines > /dev/null; then
+                echo "" > /tmp/hyda/dynamic-machines && echo "Cleared dynamic build machines"
+              fi
             fi
           '';
         };
