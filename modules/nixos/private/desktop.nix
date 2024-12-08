4df9bec798c89d210b20daeea756c39ca07f8d7f
Shawn8901 <shawn8901@googlemail.com>
2024-12-09 00:09:11 +0100

    desktop: remove unused fs prom udisk2 and partition-manager

--- modules/nixos/private/desktop.nix
+++ modules/nixos/private/desktop.nix
@@ -163,5 +163,28 @@ in
         kmail = true;
       };
     };
+
+    nixpkgs.config.packageOverrides = pkgs: {
+
+      udisks2 = pkgs.udisks2.override {
+        btrfs-progs = null;
+        nilfs-utils = null;
+        xfsprogs = null;
+        f2fs-tools = null;
+      };
+
+      partition-manager = pkgs.partition-manager.override {
+        btrfs-progs = null;
+        e2fsprogs = null;
+        f2fs-tools = null;
+        hfsprogs = null;
+        jfsutils = null;
+        nilfs-utils = null;
+        reiser4progs = null;
+        reiserfsprogs = null;
+        udftools = null;
+        xfsprogs = null;
+      };
+    };
   };
 }
